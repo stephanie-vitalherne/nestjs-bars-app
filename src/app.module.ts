3120664e59ac6b81c817e0c3138ec38fd79512fa
Stephanie Vital-Herne <vitalherne@gmail.com>
2021-09-14 23:47:06 -0400

    half set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      playground: (process.env.NODE_ENV === 'development' && true) || false,
+      debug: (process.env.NODE_ENV === 'development' && true) || false,
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
